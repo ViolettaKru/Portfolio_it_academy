98216788af380020ae88fdd0bdb8fef388cfe339
violetta Krupiankova <kruviola@gmail.com>
2020-03-04 12:42:08 +0300

    v1

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+
+$(function(){
+    $('.bxslider').bxSlider({
+        mode: 'fade',
+        captions: true,
+        slideWidth: 1000,
+        speed: 500,
+        startSlide: 0,
+        minSlides: 1
+    });
+});
+
